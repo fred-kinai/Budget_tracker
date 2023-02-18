15a3506d44ed0880f325c30be9b53fe552f5ef1e
Fred Kinai <fredkinai56@gmail.com>
2023-02-18 12:25:10 +0300

    3rd commit

--- app/index.php
+++ app/index.php
@@ -1,4 +1,10 @@
 <?php
 
-$name = 'fred';
-echo $name;
+declare(strict_types = 1);
+$root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+
+define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
+define('FILES_PATH' ,$root . 'view' . DIRECTORY_SEPARATOR);
+define('VIEWS_PATH' , $root . 'vies' . DIRECTORY_SEPARATOR);
+
+
